a0baab5e4d74b51f319ea91374cada4902843519
Jake Clifford <jakeclifford@Jakes-Air.modem>
2021-07-09 18:10:47 +1000

    Add existing file

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 import sqlite3
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required, usd
 
 app = Flask(__name__)
+app.secret_key = '242c8430e6634e4693718a30e863e2ee'
 
 #Allows Jinja to covert values to USD
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
 
 app.jinja_env.filters["usd"] = usd
 
@@ -19,10 +20,13 @@ def before_request():
     g.db.row_factory = dict_factory
 
 @app.route("/", methods=['GET', 'POST'])
+@login_required
 def index():
     if request.method == "POST":
 
         #Check input's from form are entered, store in variables.
+        
+
         if not request.form.get("subscription"):
             return redirect('/')
         else:
@@ -43,10 +47,10 @@ def index():
         else:
             pay_date = request.form.get("pay_date")
         
-        user = 1
-
+        
+        print(session["user_id"])
         #insert form variables into table
-        g.db.execute("INSERT INTO subs(sub_name, price, pay_period, pay_date, user_id) VALUES ( ?, ?, ?, ?, ?)", (subscription, price, period, pay_date, user,))
+        g.db.execute("INSERT INTO subs(sub_name, price, pay_period, pay_date, user_id) VALUES ( ?, ?, ?, ?, ?)", (subscription, price, period, pay_date, session["user_id"],))
         g.db.commit()
 
         return redirect('/')
@@ -54,8 +58,14 @@ def index():
     else:
 
         #Add each sub from database to list, pass into template where jinja syntax will display values
+        
+        users = []
+        for user in g.db.execute('SELECT * FROM users WHERE id =?', (session["user_id"],)):
+            users.append(user)
+
+
         subs = []
-        for sub in g.db.execute('SELECT * FROM subs'):
+        for sub in g.db.execute('SELECT * FROM subs WHERE user_id =? ORDER BY pay_date', (session["user_id"],)):
             subs.append(sub) 
 
 
@@ -74,7 +84,70 @@ def index():
         monthly_cost = daily_cost * 30.4
         yearly_cost = daily_cost * 365
 
-        return render_template("index.html", subs=subs, daily_cost=daily_cost, weekly_cost=weekly_cost, monthly_cost=monthly_cost, yearly_cost=yearly_cost)
+        return render_template("index.html", subs=subs, daily_cost=daily_cost, weekly_cost=weekly_cost, monthly_cost=monthly_cost, yearly_cost=yearly_cost, user=user)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+
+        #Check input's from form are entered, store in variables.
+        if not request.form.get("email"):
+            flash('Must enter email', category='error')
+            return redirect('/register')
+        else:
+            email = request.form.get("email")
+         
+        if not request.form.get("password"):
+            flash('Must enter email', category='error')
+            return redirect('/register')
+        else:
+            password = generate_password_hash(request.form.get("password"))
+
+        g.db.execute("INSERT INTO users (email, password) VALUES ( ?, ?)", (email, password,))
+        g.db.commit()
+
+        return redirect('/login')
+
+    return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+
+        session.clear()
+
+        #Check input's from form are entered, store in variables.
+        if not request.form.get("email"):
+            return redirect('/login')
+        else:
+            email = request.form.get("email")
+         
+        if not request.form.get("password"):
+            return redirect('/login')
+        else:
+            password = request.form.get("password")
+
+        users = []
+        for user in g.db.execute('SELECT * FROM users WHERE email = ?', (email,)):
+            users.append(user) 
+        
+        print(users)
+
+        if len(users) != 1 or not check_password_hash(users[0]["password"], password):
+            return redirect('/login')
+
+        session["user_id"] = users[0]["id"]
+
+        return redirect('/')
+
+        
+
+    return render_template("login.html")
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    session.clear()
+    return redirect('/login')
 
 @app.route("/delete", methods=['POST'])
 def delete():
@@ -85,6 +158,7 @@ def delete():
     return redirect('/')
 
 @app.route("/edit", methods=['POST', 'GET'])
+@login_required
 def edit():
     
     id = request.form.get('id')
